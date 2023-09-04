f86b2709112ca40faae26ce4b1eaf1e227996d27
Atasik <Atasikgames@gmail.com>
2023-09-04 03:32:56 +0300

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -23,21 +23,20 @@ type (
 	}
 
 	PostgresConfig struct {
-		Username        string
-		Password        string
-		Port            string
-		Host            string
-		DBName          string        `mapstructure:"dbname"`
-		SSLMode         string        `mapstructure:"sslmode"`
-		RefreshInterval time.Duration `mapstructure:"refreshInterval"`
+		User     string
+		Password string
+		Port     string
+		Host     string
+		DBName   string `mapstructure:"dbname"`
+		SSLMode  string `mapstructure:"sslmode"`
 	}
 
 	HTTPConfig struct {
-		Host           string
-		Port           string        `mapstructure:"port"`
-		ReadTimeout    time.Duration `mapstructure:"readTimeout"`
-		WriteTimeout   time.Duration `mapstructure:"writeTimeout"`
-		MaxHeaderBytes int           `mapstructure:"maxHeaderMegaBytes"`
+		Host               string
+		Port               string        `mapstructure:"port"`
+		ReadTimeout        time.Duration `mapstructure:"readTimeout"`
+		WriteTimeout       time.Duration `mapstructure:"writeTimeout"`
+		MaxHeaderMegaBytes int           `mapstructure:"maxHeaderMegaBytes"`
 	}
 
 	AuthConfig struct {
@@ -51,11 +50,11 @@ type (
 	}
 
 	Argon2Config struct {
-		Memory      uint32
-		Iterations  uint32
-		Parallelism uint8
-		SaltLength  uint32
-		KeyLength   uint32
+		MemoryMegaBytes uint32 `mapstructure:"memory"`
+		Iterations      uint32 `mapstructure:"iterations"`
+		Parallelism     uint8  `mapstructure:"parallelism"`
+		SaltLength      uint32 `mapstructure:"saltLength"`
+		KeyLength       uint32 `mapstructure:"keyLength"`
 	}
 
 	CloudinaryConfig struct {
@@ -87,18 +86,29 @@ func unmarshal(cfg *Config) error {
 		return err
 	}
 
+	if err := viper.UnmarshalKey("auth", &cfg.Auth.JWT); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("hash", &cfg.Auth.Argon2); err != nil {
+		return err
+	}
+
 	return viper.UnmarshalKey("http", &cfg.HTTP)
 }
 
 func setFromEnv(cfg *Config) {
 	cfg.Postgres.Password = os.Getenv("POSTGRES_PASSWORD")
-	cfg.Postgres.Username = os.Getenv("POSTGRES_USER")
+	cfg.Postgres.User = os.Getenv("POSTGRES_USER")
 	cfg.Postgres.Port = os.Getenv("POSTGRES_PORT")
 	cfg.Postgres.Host = os.Getenv("POSTGRES_HOST")
+
 	cfg.HTTP.Host = os.Getenv("HTTP_HOST")
 	cfg.Cloudinary.Cloud = os.Getenv("CLOUDINARY_CLOUD")
 	cfg.Cloudinary.Key = os.Getenv("CLOUDINARY_KEY")
 	cfg.Cloudinary.Secret = os.Getenv("CLOUDINARY_SECRET")
+
+	cfg.Auth.JWT.SigningKey = os.Getenv("JWT_SIGNING_KEY")
 }
 
 func parseConfigFile(folder string) error {
@@ -117,5 +127,4 @@ func setDefaults() {
 	viper.SetDefault("http.maxHeaderMegaBytes", defaultHTTPMaxHeaderMegabytes)
 	viper.SetDefault("http.readTimeout", defaultHTTPRWTimeout)
 	viper.SetDefault("http.writeTimeout", defaultHTTPRWTimeout)
-	viper.SetDefault("postgres.refreshInterval", defaultDatabaseRefreshInterval)
 }
